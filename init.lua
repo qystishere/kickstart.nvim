a1e04dfb1c8b7750ab589ab2f3ccc8347b56c477
qystishere <qystishere@gmail.com>
2024-05-28 10:36:58 +0300

    Добавил расширение для синтаксиса Jai

--- init.lua
+++ init.lua
@@ -234,6 +234,9 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
+  -- NOTE: Поддержка Jai
+  'jansedivy/jai.vim',
+
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.

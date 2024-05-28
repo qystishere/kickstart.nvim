74066643638b75104c23fbc865c007c90edbe393
qystishere <qystishere@gmail.com>
2024-05-28 10:23:03 +0300

    Ширина таба

--- init.lua
+++ init.lua
@@ -154,6 +154,12 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Играюсь
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
